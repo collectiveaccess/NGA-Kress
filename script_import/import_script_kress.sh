bf0be307ab9ed555190fa7f57a66566c97eb05a3
eveperry <55102640+eveperry@users.noreply.github.com>
2020-03-04 17:41:00 -0500

    Import Script

---
+++ script_import/import_script_kress.sh
@@ -0,0 +1,36 @@
+######Run Using sudo bash import_script_kress.sh
+
+
+
+#echo "Importing name authorities..."
+#bin/caUtils import-data --format=XLSX  --mapping=NA01 --source=02_NameAuthorities_edited.xlsx --log-level=ERR --log=import --direct 
+#&1
+
+echo "Importing object acquisition join..."
+bin/caUtils import-data --format=XLSX  --mapping=object_acquisition --source=05_AcquisitionObjectDetail.xlsx --log-level=ERR --log=import --direct 
+&1
+
+echo "Importing artwork..."
+bin/caUtils import-data --format=XLSX  --mapping=AO01 --source=01_ArtObjects.xlsx --log-level=ERR --log=import --direct 
+&1
+
+echo "Importing documents..."
+bin/caUtils import-data --format=XLSX  --mapping=document --source=03_Documents_edited.xlsx --log-level=ERR --log=import --direct 
+&1
+
+echo "Importing distributions..."
+bin/caUtils import-data --format=XLSX  --mapping=DO01 --source=06_Distrubutions_ValuesOnly.xlsx --log-level=ERR --log=import --direct 
+&1
+
+echo "Importing acquisitions..."
+bin/caUtils import-data --format=XLSX  --mapping=acquisition --source=04_Acquisitions_Group.xlsx --log-level=ERR --log=import --direct 
+&1
+
+mysqldump -uroot kress2 >  /data/kress2/support/backup_prototype_2_1.dump>
+
+#cd /data/kress2/support/scripts
+#php ImportMedia.php
+
+
+#cd /data/kress2/support/
+bin/caUtils rebuild-search-index
