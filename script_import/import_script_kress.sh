62aace56c4853ea2c5603e806e2824fa0aed16bc
eveperry <55102640+eveperry@users.noreply.github.com>
2020-03-05 16:19:13 -0500

    Update import_script_kress.sh

--- script_import/import_script_kress.sh
+++ script_import/import_script_kress.sh
@@ -2,9 +2,9 @@
 
 
 
-#echo "Importing name authorities..."
-#bin/caUtils import-data --format=XLSX  --mapping=NA01 --source=02_NameAuthorities_edited.xlsx --log-level=ERR --log=import --direct 
-#&1
+echo "Importing name authorities..."
+bin/caUtils import-data --format=XLSX  --mapping=NA01 --source=02_NameAuthorities_edited.xlsx --log-level=ERR --log=import --direct 
+&1
 
 echo "Importing object acquisition join..."
 bin/caUtils import-data --format=XLSX  --mapping=object_acquisition --source=05_AcquisitionObjectDetail.xlsx --log-level=ERR --log=import --direct 
@@ -26,11 +26,11 @@ echo "Importing acquisitions..."
 bin/caUtils import-data --format=XLSX  --mapping=acquisition --source=04_Acquisitions_Group.xlsx --log-level=ERR --log=import --direct 
 &1
 
-mysqldump -uroot kress2 >  /data/kress2/support/backup_prototype_2_1.dump>
+mysqldump -uroot kress2 >  /data/kress2/support/backup_prototype_2_2.dump
 
-#cd /data/kress2/support/scripts
-#php ImportMedia.php
+cd /data/kress2/support/scripts
+php ImportMedia.php
 
 
-#cd /data/kress2/support/
+cd /data/kress2/support/
 bin/caUtils rebuild-search-index
