18acf9afac54bafec523019c3fbf8fa27016c663
eveperry <55102640+eveperry@users.noreply.github.com>
2020-03-30 18:22:47 -0400

    change to csv

--- script_import/import_script_kress.sh
+++ script_import/import_script_kress.sh
@@ -3,27 +3,27 @@
 
 
 echo "Importing name authorities..."
-bin/caUtils import-data --format=XLSX  --mapping=NA01 --source=02_NameAuthorities_edited.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=NA01 --source=02_NameAuthorities_edited.xlsx --log-level=ERR --log=import --direct 
 
 
 echo "Importing object acquisition join..."
-bin/caUtils import-data --format=XLSX  --mapping=object_acquisition --source=05_AcquisitionObjectDetail.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=object_acquisition --source=05_AcquisitionObjectDetail.xlsx --log-level=ERR --log=import --direct 
 
 
 echo "Importing artwork..."
-bin/caUtils import-data --format=XLSX  --mapping=AO01 --source=01_ArtObjects.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=AO01 --source=01_ArtObjects.xlsx --log-level=ERR --log=import --direct 
 
 
 echo "Importing documents..."
-bin/caUtils import-data --format=XLSX  --mapping=document --source=03_Documents_edited.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=document --source=03_Documents_edited.xlsx --log-level=ERR --log=import --direct 
 
 
 echo "Importing distributions..."
-bin/caUtils import-data --format=XLSX  --mapping=DO01 --source=06_Distrubutions_ValuesOnly.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=DO01 --source=06_Distributions_ValuesOnly.xlsx --log-level=ERR --log=import --direct 
 
 
 echo "Importing acquisitions..."
-bin/caUtils import-data --format=XLSX  --mapping=acquisition --source=04_Acquisitions_Group.xlsx --log-level=ERR --log=import --direct 
+bin/caUtils import-data --format=CSV  --mapping=acquisition --source=04_Acquisitions_Group.xlsx --log-level=ERR --log=import --direct 
 
 
 
@@ -31,7 +31,7 @@ bin/caUtils import-data --format=XLSX  --mapping=acquisition --source=04_Acquisi
 mysqldump -uroot -ppassword kress2 >  /data/kress2/support/backup_prototype_2_3.dump
 
 cd /data/kress2/support/scripts
-phpfixItalics.php
+php fixItalics.php
 php importMedia.php
 
 
