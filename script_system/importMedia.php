3efd82925a76e10cc9df11abc2168cd7b93ddf03
mariamotorina <maria@whirl-i-gig.com>
2020-08-04 13:47:39 -0400

    revised media import for multiple files per object

--- script_system/importMedia.php
+++ script_system/importMedia.php
@@ -1,31 +1,37 @@
 <?php
-	require_once("../setup.php");
+	require_once("../../setup.php");
 	
-	$p = DelimitedDataParser::load("01_ArtObjects.xlsx");
+	$p = DelimitedDataParser::load("../01_ArtObjects.csv", ['delimiter' => ',']);
 	
-	$files = caGetDirectoryContentsAsList("../import");
+	$files = caGetDirectoryContentsAsList("../../import/KressCollectionImages_20200220");
 	
 	
 	foreach($files as $f) {
 		$files[pathinfo($f, PATHINFO_BASENAME)] = $f;
 	}
-	
 	while($p->nextRow()) {
 		$r = $p->getRow();
 		
 		$id = $r[0];
-		$fname = $r[33];
+		$fnames = $r[33];
 		
-		if ($fname) {
-			if($files[$fname]) {
-				if ($o = ca_objects::find(['idno' => $id], ['returnAs' => 'firstModelInstance'])) {
-					if ($o->getRepresentationCount() == 0) {
-						print "$id => $files[$fname]\n";
-						$o->addRepresentation($files[$fname], 'front', 1, 0, 0, true);
-						DataMigrationUtils::postError($o, "While adding media");
+		if ($fnames) {
+			if ($o = ca_objects::find(['idno' => $id], ['returnAs' => 'firstModelInstance'])) {
+				if ($o->getRepresentationCount() == 0) {
+					$va_file_names = explode(";", $fnames);
+					$c = 1;
+					foreach($va_file_names as $fname){
+						$fname = trim($fname);
+						if($files[$fname]) {
+							print "$id => $files[$fname]\n";
+							$o->addRepresentation($files[$fname], 'front', 1, 0, 1, ($c > 1) ? false : true);
+							DataMigrationUtils::postError($o, "While adding media");
+							$c++;
+						}
 					}
 				}
 			}
 			
 		}
 	}
+?>
